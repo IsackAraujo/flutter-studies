c0b62433a9c2c1201715aa5ac93064d4a1d082e4
IsackAraujo <isackaraujoc@gmail.com>
2024-04-28 21:01:20 -0300

    Stateless Type

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,49 @@ main (){
 class PerguntaApp extends StatelessWidget{
 
   Widget build(BuildContext context){
+
+    var perguntaSelecionada = 0;
+    
+    void responder(){
+      perguntaSelecionada++;
+      print(perguntaSelecionada);
+    }
+
+    void Function() functionThatWillReturnAnotherFunction() {
+      return () {
+        print("Im Printing Something!");
+      };
+    }
+
+    final perguntas = [
+      "Qual é o seu nome?",
+      "Qual é o nome de seu Cachorro?"
+       ];
+    
     return MaterialApp(
-      home: Text("Imperatrizz"),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text("Vamo lá Guerreiro"),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[perguntaSelecionada]),
+            ElevatedButton(
+              onPressed: responder,
+              child: Text("Resposta 1")
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: Text("Resposta 2")
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: Text("Resposta 3")
+            )
+          ],
+        )
+      )
     );
   }
 }
