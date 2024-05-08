b203d951c2c3dc61626975e82b0c33e3b6774f9b
IsackAraujo <isackaraujoc@gmail.com>
2024-05-08 01:04:50 -0300

    Navigation inside classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,49 @@
-
 import 'package:flutter/material.dart';
-import 'package:teste/models/mysql.dart';
+import 'package:flutter/widgets.dart';
+import 'package:teste/testeScreen.dart';
 import './questao.dart';
 
-main (){
-  runApp(new PerguntaApp());
+  void main() {
+  runApp(MaterialApp(
+    home: PerguntaApp(),
+  ));
+
 }
 
-class Student{
-
+class Student {
   String? name;
   int? age;
 
   Student({this.name, this.age});
 
-  printStudent(){
-    print("Printing a studend in the console!!! Name: ${this.name}, Age: ${this.age}");
+  printStudent() {
+    print("Printing a studend in the console!!! Name: $name, Age: $age");
   }
 }
-class PerguntaAppState extends State<PerguntaApp> {
 
+class PerguntaAppState extends State<PerguntaApp> {
   var email = '';
 
+  var perguntaSelecionada = 0;
 
-    var perguntaSelecionada = 0;
+  final perguntas = ["Qual é o seue nome?", "Qual é o nome de seu Cachorro?"];
 
-    final perguntas = [
-      "Qual é o seu nome?",
-      "Qual é o nome de seu Cachorro?"
-    ];
-    
-    void responder(){
-      setState(() {
-        var aluno = new Student(
-          name: "Carl",
-          age: 14
-        );
+  void responder() {
+    setState(() {
+      var aluno = Student(name: "Carl", age: 14);
 
-      if(perguntaSelecionada == perguntas.length-1){
-      aluno.printStudent();
-      perguntaSelecionada = 0;
-      }else{
-      perguntaSelecionada++;
+      if (perguntaSelecionada == perguntas.length - 1) {
+        aluno.printStudent();
+        perguntaSelecionada = 0;
+      } else {
+        perguntaSelecionada++;
       }
-      });
-      print(perguntaSelecionada);
-    }
+    });
+    print(perguntaSelecionada);
+  }
 
-      Widget build(BuildContext context){
-
+  @override
+  Widget build(BuildContext context) {
     void Function() functionThatWillReturnAnotherFunction() {
       return () {
         print("Im Printing Something!");
@@ -60,43 +55,32 @@ class PerguntaAppState extends State<PerguntaApp> {
         print("Label was tapped");
       };
     }
- 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Center(
-            child: Text("MAIL: ${email}")
-            ),
-        ),
-        body: Column(
-          children: [
-            Divider(
-              height: 30,
-              ),
 
-            Container(
-              width: double.infinity,
-              margin: EdgeInsets.all(10),
-              child: Text(
-                "In this example we have two questions, and when you click in some buttom, you change the question, and every time that you achieved the second question, you will come back to the first and create a instance of a class Student and print it on the console.",
-                style: TextStyle(
-                   fontSize: 12
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.blue,
+              title: Center(child: Text("MAIL: $email")),
+            ),
+            body: Column(
+              children: [
+                Divider(
+                  height: 30,
                 ),
-                textAlign: TextAlign.center,
+
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "In this example we have two questions, and when you click in some buttom, you change the question, and every time that you achieved the second question, you will come back to the first and create a instance of a class Student and print it on the console.",
+                    style: TextStyle(fontSize: 12),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-            ),
-            Questao(texto: perguntas[perguntaSelecionada]),
-            ElevatedButton(
-              onPressed: responder,
-              child: Text("Resposta 1")
-            ),
-            ElevatedButton(
-              onPressed: responder,
-              child: Text("Resposta 2")
-            ),
-            ElevatedButton(
-              onPressed: responder, child: Text("Resposta 3")),
+                Questao(texto: perguntas[perguntaSelecionada]),
+                ElevatedButton(onPressed: responder, child: Text("Resposta 1")),
+                ElevatedButton(onPressed: responder, child: Text("Resposta 2")),
+                ElevatedButton(onPressed: responder, child: Text("Resposta 3")),
                 TextField(
                   decoration: InputDecoration(
                       border: UnderlineInputBorder(),
@@ -106,22 +90,35 @@ class PerguntaAppState extends State<PerguntaApp> {
                   autocorrect: true,
                   onTap: printOnTap(),
                 ),
-            ElevatedButton(
-              onPressed: printOnTap(),
-               child: Icon(Icons.person)
-               ),
-          ],
-        )
-      )
-    );
-  }
+                ElevatedButton(
+                    onPressed: printOnTap(), child: Icon(Icons.person)),
 
+                SizedBox(
+                  width: 20,
+                ),
+
+                // Another screen buttom
+
+                Center(
+                    child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => TestScreen()),
+                          );
+                        },
+                        child: const Text(
+                          "Another Screen",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )))
+              ],
+            )));
+  }
 }
 
 class PerguntaApp extends StatefulWidget {
-
   PerguntaAppState createState() {
-    return PerguntaAppState(); 
+    return PerguntaAppState();
   }
-
 }
---
+++ lib/testeScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// Class create to exercise the process of navigating in classes
+
+class TestScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Test Screen"),
+      ),
+    );
+  }
+
+}
