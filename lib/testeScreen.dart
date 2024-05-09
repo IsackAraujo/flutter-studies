10d5b3d011a30451755c48a3f97751362d8f2044
IsackAraujo <isackaraujoc@gmail.com>
2024-05-08 23:35:47 -0300

    Creating new Screen

--- lib/testeScreen.dart
+++ lib/testeScreen.dart
@@ -3,15 +3,45 @@ import 'package:flutter/material.dart';
 // Class create to exercise the process of navigating in classes
 
 class TestScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Test Screen"),
-      ),
-    );
-  }
+        body: SafeArea(
+      child: Column(
+        children: [
+          Row(
+            
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+             children: [
+            Container(
+              padding: EdgeInsets.all(25),
+               child: Text("Olá Flutter!",
+               style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+               ),
+               )
+               ),
 
+            Container(
+              padding: EdgeInsets.all(25),
+              child: Icon(
+              
+              Icons.back_hand
+              
+              )
+            )   
+          ]),
+
+          Column(
+            children: [
+              Padding(padding: EdgeInsets.all(25),
+              child: Text("Building Screen..."),
+              )
+            ],
+          )
+        ],
+      ),
+    ));
+  }
 }
